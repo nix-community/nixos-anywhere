b0a2740aca21dad4ed655c3a001da4fde8025bb9
Jörg Thalheim <joerg@thalheim.io>
2022-12-09 15:03:50 +0100

    package: include dependencies in wrapper

--- package.nix
+++ package.nix
@@ -1,8 +1,21 @@
-{ stdenvNoCC }:
+{ stdenvNoCC, makeWrapper, lib, openssh, git, nix, coreutils }:
+let
+  runtimeDeps = [
+    openssh
+    git # for git flakes
+    nix
+    coreutils
+  ];
+in
 stdenvNoCC.mkDerivation {
   name = "nixos-remote";
   src = ./.;
+  nativeBuildInputs = [
+    makeWrapper
+  ];
   installPhase = ''
     install -D -m755 nixos-remote $out/bin/nixos-remote
+    wrapProgram "$out/bin/nixos-remote" \
+        --prefix PATH : "${lib.makeBinPath runtimeDeps}"
   '';
 }
