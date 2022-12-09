26258d1b2455d7bbab61fb6a0289545a9460334a
Jörg Thalheim <joerg@thalheim.io>
2022-12-09 14:56:06 +0100

    add flake for nixos-remote

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Install nixos everywhere via ssh";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs, ... }: let
+    supportedSystems = [
+      "x86_64-linux"
+      "i686-linux"
+      "aarch64-linux"
+    ];
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+  in {
+    packages = forAllSystems (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      nixos-remote = pkgs.callPackage ./package.nix {};
+      default = self.packages.${system}.nixos-remote;
+    });
+  };
+}
