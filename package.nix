32ba35c4ee7eaba2bf08507d69133b972e2bccbe
zimbatm <zimbatm@zimbatm.com>
2022-12-21 18:53:39 +0100

    add shellcheck test

--- package.nix
+++ package.nix
@@ -1,4 +1,12 @@
-{ stdenvNoCC, makeWrapper, lib, openssh, gitMinimal, nix, coreutils }:
+{ stdenvNoCC
+, makeWrapper
+, lib
+, openssh
+, gitMinimal
+, nix
+, coreutils
+, shellcheck
+}:
 let
   runtimeDeps = [
     openssh
@@ -12,10 +20,16 @@ stdenvNoCC.mkDerivation {
   src = ./.;
   nativeBuildInputs = [
     makeWrapper
+    shellcheck
   ];
   installPhase = ''
     install -D -m755 nixos-remote $out/bin/nixos-remote
     wrapProgram "$out/bin/nixos-remote" \
         --prefix PATH : "${lib.makeBinPath runtimeDeps}"
+  '';
+
+  doCheck = true;
+  checkPhase = ''
+    shellcheck ./nixos-remote
   '';
 }
