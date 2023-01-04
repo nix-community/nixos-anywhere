d57dc3d7501a412181e5b3b3bad4f4676ea667ab
phaer <hello@phaer.org>
2023-01-04 20:02:23 +0100

    allow pipes for --disk-encryption-keys...
    this allows passing multiple disk encryption keys, some of which
    might come in the form of unix pipes.
    
    It can be used with bash file substition facilities to pass a secret
    to the remote machine without writing it locally to disk.
    
    Example
    
    --disk-encryption-keys /tmp/disk-1.key <(echo "my-secret")
    --disk-encryption-keys /tmp/disk-2.key /tmp/static-file.key

--- tests/from-nixos-with-sudo.nix
+++ tests/from-nixos-with-sudo.nix
@@ -6,21 +6,24 @@
   };
   testScript = ''
     start_all()
-    installer.succeed("echo super-secret > /tmp/disk-encryption-key")
+    installer.succeed("echo super-secret > /tmp/disk-1.key")
     output = installer.succeed("""
       ${../nixos-remote} \
         --no-ssh-copy-id \
         --debug \
         --kexec /etc/nixos-remote/kexec-installer \
         --stop-after-disko \
-        --disk-encryption-keys /tmp/disk-encryption-key \
+        --disk-encryption-keys /tmp/disk-1.key /tmp/disk-1.key \
+        --disk-encryption-keys /tmp/disk-2.key <(echo another-secret) \
         --store-paths /etc/nixos-remote/disko /etc/nixos-remote/system-to-install \
         nixos@installed >&2
-      key=$(ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
-        root@installed cat /tmp/disk-encryption-key)
-      echo "encryption key: '$key'"
+      echo "disk-1.key: '$(ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
+        root@installed cat /tmp/disk-1.key)'"
+      echo "disk-2.key: '$(ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
+        root@installed cat /tmp/disk-2.key)'"
     """)
 
-    assert "encryption key: 'super-secret'" in output, f"output does not contain expected values: {output}"
+    assert "disk-1.key: 'super-secret'" in output, f"output does not contain expected values: {output}"
+    assert "disk-2.key: 'another-secret'" in output, f"output does not contain expected values: {output}"
   '';
 }
