5a158efb9b9d1ff7c65aff8cfb96892fcb81eee7
phaer <hello@phaer.org>
2023-01-21 15:38:09 +0100

    commit broken syntax...
    ...just to stop CI until i have time to fix this properly ;)

--- tests/flake-module.nix
+++ tests/flake-module.nix
@@ -10,6 +10,7 @@
         kexec-installer = builtins.fetchurl {
           url = "https://github.com/dep-sys/nix-dabei/releases/download/v0.5/nixos-kexec-installer-x86_64-linux.tar.gz";
           sha256 = "sha256:18b0mb714jzfrpvg19bw77h16s78ig8l24mqnrx4z73gzlfvrz7g";
+          break it
         };
       };
     in
