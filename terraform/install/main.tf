ac7f3b86a51f6d031098e4c1018c42700b27b2b0
Jörg Thalheim <joerg@thalheim.io>
2023-02-22 16:42:36 +0100

    terraform: actually passthru kexec-url to nixos-anywhere

--- terraform/install/main.tf
+++ terraform/install/main.tf
@@ -1,8 +1,12 @@
+locals {
+  nixos_anywhere_flags = "${var.debug_logging ? "--debug" : ""} ${var.kexec_tarball_url != "" ? "--kexec ${var.kexec_tarball_url}" : "" } --store-paths ${var.nixos_partitioner} ${var.nixos_system} ${var.target_user}@${var.target_host}"
+}
+
 resource "null_resource" "nixos-remote" {
   provisioner "local-exec" {
     environment = {
       SSH_PRIVATE_KEY = var.ssh_private_key
     }
-    command = "nix run --extra-experimental-features 'nix-command flakes' ${path.module}#nixos-anywhere -- ${var.debug_logging ? "--debug" : ""} --store-paths ${var.nixos_partitioner} ${var.nixos_system} ${var.target_user}@${var.target_host}"
+    command = "nix run --extra-experimental-features 'nix-command flakes' ${path.module}#nixos-anywhere -- ${local.nixos_anywhere_flags}"
   }
 }
