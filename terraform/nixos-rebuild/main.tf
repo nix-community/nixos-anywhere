c2f70ed8449461b02fec74c448a0ef9c616b53af
Jörg Thalheim <joerg@thalheim.io>
2023-03-03 14:47:03 +0100

    nixos-rebuild: allow to change deploy user

--- terraform/nixos-rebuild/main.tf
+++ terraform/nixos-rebuild/main.tf
@@ -3,6 +3,6 @@ resource "null_resource" "nixos-rebuild" {
     store_path = var.nixos_system
   }
   provisioner "local-exec" {
-    command = "${path.module}/deploy.sh ${var.nixos_system} root@${var.target_host} ${var.target_port}"
+    command = "${path.module}/deploy.sh ${var.nixos_system} ${var.target_user}@${var.target_host} ${var.target_port}"
   }
 }
--- terraform/nixos-rebuild/variables.tf
+++ terraform/nixos-rebuild/variables.tf
@@ -8,6 +8,12 @@ variable "target_host" {
   description = "DNS host to deploy to"
 }
 
+variable "target_user" {
+  type        = string
+  default     = "root"
+  description = "User to deploy as"
+}
+
 variable "target_port" {
   type        = number
   description = "SSH port used to connect to the target_host"
