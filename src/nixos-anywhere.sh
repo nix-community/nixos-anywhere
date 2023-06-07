69443d692920ff11b196980fb984b03ef1bcb2d4
Jörg Thalheim <joerg@thalheim.io>
2023-06-07 07:11:27 +0200

    make usage formatting more consistent

--- src/nixos-anywhere.sh
+++ src/nixos-anywhere.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 showUsage() {
   cat <<USAGE
-Usage: nixos-anywhere [options] ssh-host
+Usage: nixos-anywhere [options] <ssh-host>
 
 Options:
 
@@ -13,27 +13,27 @@ Options:
   selects which SSH private key file to use.
 * -L, --print-build-logs
   print full build logs
-* -s, --store-paths
+* -s, --store-paths <disko-script> <nixos-system>
   set the store paths to the disko-script and nixos-system directly
   if this is give, flake is not needed
 * --no-reboot
   do not reboot after installation, allowing further customization of the target installation.
-* --kexec url
+* --kexec <url>
   use another kexec tarball to bootstrap NixOS
 * --stop-after-disko
   exit after disko formating, you can then proceed to install manually or some other way
-* --extra-files files
+* --extra-files <file...>
   files to copy into the new nixos installation
-* --disk-encryption-keys remote_path local_path
+* --disk-encryption-keys <remote_path> <local_path>
   copy the contents of the file or pipe in local_path to remote_path in the installer environment,
   after kexec but before installation. Can be repeated.
 * --no-substitute-on-destination
   disable passing --substitute-on-destination to nix-copy
 * --debug
   enable debug output
-* --option KEY VALUE
+* --option <key> <value>
   nix option to pass to every nix related command
-* --from store-uri
+* --from <store-uri>
   URL of the source Nix store to copy the nixos and disko closure from
 * --build-on-remote
   build the closure on the remote machine instead of locally and copy-closuring it
