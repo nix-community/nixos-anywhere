b6ef45710797bf428deeb4cc39e475ffa07e507a
Jörg Thalheim <joerg@thalheim.io>
2023-09-17 10:03:17 +0200

    make treefmt-nix an optional dependencies

--- flake.nix
+++ flake.nix
@@ -4,12 +4,14 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
     flake-parts = { url = "github:hercules-ci/flake-parts"; inputs.nixpkgs-lib.follows = "nixpkgs"; };
-    disko = { url = "github:nix-community/disko/master"; inputs.nixpkgs.follows = "nixpkgs"; };
+
     # used for testing
+    disko = { url = "github:nix-community/disko/master"; inputs.nixpkgs.follows = "nixpkgs"; };
     nixos-2305.url = "github:NixOS/nixpkgs/release-23.05";
     nixos-images.url = "github:nix-community/nixos-images";
     nixos-images.inputs.nixos-unstable.follows = "nixpkgs";
     nixos-images.inputs.nixos-2305.follows = "nixos-2305";
+
     # used for development
     treefmt-nix = { url = "github:numtide/treefmt-nix"; inputs.nixpkgs.follows = "nixpkgs"; };
   };
@@ -22,22 +24,10 @@
         ./src/flake-module.nix
         ./tests/flake-module.nix
         ./docs/flake-module.nix
-        inputs.treefmt-nix.flakeModule
-      ];
+        # allow to disable treefmt in downstream flakes
+      ] ++ inputs.nixpkgs.lib.optional (inputs.treefmt-nix ? flakeModule) ./treefmt/flake-module.nix;
 
       perSystem = { self', config, lib, ... }: {
-        treefmt = {
-          projectRootFile = "flake.nix";
-          programs.mdsh.enable = true;
-          programs.nixpkgs-fmt.enable = true;
-          programs.shellcheck.enable = true;
-          programs.shfmt.enable = true;
-          programs.prettier.enable = true;
-          settings.formatter.prettier.options = [ "--prose-wrap" "always" ];
-          settings.formatter.shellcheck.options = [ "-s" "bash" ];
-        };
-        formatter = config.treefmt.build.wrapper;
-
         checks =
           let
             packages = lib.mapAttrs' (n: lib.nameValuePair "package-${n}") self'.packages;
