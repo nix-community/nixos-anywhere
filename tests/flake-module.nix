3a87ed3cb058567cff3afe42f84483926d9d05a1
Jörg Thalheim <Mic92@users.noreply.github.com>
2023-12-12 15:27:17 +0100

    use stable suffix for the last nixos release (#262)
    flake.lock: Update
    
    Flake lock file updates:
    
    • Updated input 'disko':
        'github:nix-community/disko/944d338d24a9d043a3f7461c30ee6cfe4f9cca30' (2023-10-27)
      → 'github:nix-community/disko/1144887c6f4d2dcbb2316a24364ef53e25b0fcfe' (2023-12-06)
    • Updated input 'flake-parts':
        'github:hercules-ci/flake-parts/8c9fa2545007b49a5db5f650ae91f227672c3877' (2023-11-01)
      → 'github:hercules-ci/flake-parts/34fed993f1674c8d06d58b37ce1e0fe5eebcb9f5' (2023-12-01)
    • Removed input 'nixos-2305'
    • Updated input 'nixos-images':
        'github:nix-community/nixos-images/87bccdbdfbeb07e0c4fb4c2ec3d71986e8fa24d9' (2023-10-17)
      → 'github:nix-community/nixos-images/d655cc02fcb9ecdcca4f3fb307e291a4b5be1339' (2023-12-12)
    • Removed input 'nixos-images/nixos-2305'
    • Added input 'nixos-images/nixos-2311':
        follows 'nixos-stable'
    • Added input 'nixos-stable':
        'github:NixOS/nixpkgs/cf28ee258fd5f9a52de6b9865cdb93a1f96d09b7' (2023-12-12)
    • Updated input 'nixpkgs':
        'github:nixos/nixpkgs/2542afeb0d81db4d1e5bc8df8354cbdcc29c2f53' (2023-11-17)
      → 'github:nixos/nixpkgs/120a26f8ce32ac2bdc0e49a9fed830b7446416b4' (2023-12-11)
    • Updated input 'treefmt-nix':
        'github:numtide/treefmt-nix/5deb8dc125a9f83b65ca86cf0c8167c46593e0b1' (2023-10-27)
      → 'github:numtide/treefmt-nix/5ff2cdbe0db6a6f3445f7d878cb87d121d914d83' (2023-12-11)
    
    flake.lock: Update
    
    Flake lock file updates:
    
    • Removed input 'nixos-2311'
    • Updated input 'nixos-images':
        'github:nix-community/nixos-images/b7719bef5d25f99297bc98c70db78d95e9eb2f71' (2023-12-10)
      → 'github:nix-community/nixos-images/d655cc02fcb9ecdcca4f3fb307e291a4b5be1339' (2023-12-12)
    • Updated input 'nixos-images/nixos-2311':
        follows 'nixos-2311'
      → follows 'nixos-stable'
    • Added input 'nixos-stable':
        'github:NixOS/nixpkgs/781e2a9797ecf0f146e81425c822dca69fe4a348' (2023-12-10)
    • Updated input 'nixpkgs':
        'github:nixos/nixpkgs/82787a00628b1c5cabd0d43a657c6304b8451620' (2023-12-09)
      → 'github:nixos/nixpkgs/120a26f8ce32ac2bdc0e49a9fed830b7446416b4' (2023-12-11)
    • Updated input 'treefmt-nix':
        'github:numtide/treefmt-nix/afdd5e48a0869b389027307652a658051c0d2f96' (2023-12-10)
      → 'github:numtide/treefmt-nix/390018a9398f9763bfc05ffe6443ce0622cb9ba6' (2023-12-12)

--- tests/flake-module.nix
+++ tests/flake-module.nix
@@ -3,20 +3,20 @@
 {
   flake.checks.x86_64-linux = withSystem "x86_64-linux" ({ pkgs, system, inputs', config, ... }:
     let
-      testInputs = {
+      testInputsUnstable = {
         inherit pkgs;
         inherit (inputs.disko.nixosModules) disko;
         nixos-anywhere = config.packages.nixos-anywhere;
         kexec-installer = "${inputs'.nixos-images.packages.kexec-installer-nixos-unstable-noninteractive}/nixos-kexec-installer-noninteractive-${system}.tar.gz";
       };
-      testInputs2311 = testInputs // {
+      testInputsStable = testInputsUnstable // {
         kexec-installer = "${inputs'.nixos-images.packages.kexec-installer-nixos-2311-noninteractive}/nixos-kexec-installer-noninteractive-${system}.tar.gz";
       };
     in
     {
-      from-nixos = import ./from-nixos.nix testInputs;
-      from-nixos-2311 = import ./from-nixos.nix testInputs2311;
-      from-nixos-with-sudo = import ./from-nixos-with-sudo.nix testInputs;
-      from-nixos-with-sudo-2311 = import ./from-nixos-with-sudo.nix testInputs2311;
+      from-nixos = import ./from-nixos.nix testInputsUnstable;
+      from-nixos-stable = import ./from-nixos.nix testInputsStable;
+      from-nixos-with-sudo = import ./from-nixos-with-sudo.nix testInputsUnstable;
+      from-nixos-with-sudo-stable = import ./from-nixos-with-sudo.nix testInputsStable;
     });
 }
