62e554d17764bfc382eb54f8710ebb686522c162
Sizhe Zhao <prc.zhao@outlook.com>
2024-05-09 18:16:01 +0800

    feat(src/nixos-anywhere): don't use rsync

--- src/nixos-anywhere.sh
+++ src/nixos-anywhere.sh
@@ -479,11 +479,8 @@ if [[ -n ${extra_files-} ]]; then
     extra_files="$extra_files/"
   fi
   step Copying extra files
-  rsync -rlpv -FF \
-    -e "ssh -i \"$ssh_key_dir\"/nixos-anywhere -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ${ssh_args[*]}" \
-    "$extra_files" \
-    "${ssh_connection}:/mnt/"
-  ssh_ "chmod 755 /mnt" # rsync also changes permissions of /mnt
+  tar -C "$extra_files" -cpf- . | ssh_ "${maybe_sudo} tar -C /mnt -xf-"
+  ssh_ "chmod 755 /mnt" # tar also changes permissions of /mnt
 fi
 
 step Installing NixOS
