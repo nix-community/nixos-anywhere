43909cf4494b94890ff6cd9d533f7b4af536c9fe
misumisumi <dragon511southern@gmail.com>
2024-06-16 01:45:55 +0900

    fix: format to json format

--- terraform/nix-build/nix-build.sh
+++ terraform/nix-build/nix-build.sh
@@ -3,10 +3,13 @@ set -efu
 
 declare file attribute nix_options
 eval "$(jq -r '@sh "attribute=\(.attribute) file=\(.file) nix_options=\(.nix_options)"')"
-if [[ -n ${file-} ]] && [[ -e ${file-} ]]; then
-  out=$(nix build --no-link --json $(echo "$nix_options") -f "$file" "$attribute")
+options=$(echo "${nix_options}" | jq -r '.options | to_entries | map("--option \(.key) \(.value)") | join(" ")')
+if [[ -n ${file-} ]] && [[ -e ${fileh-} ]]; then
+  # shellcheck disable=SC2086
+  out=$(nix build --no-link --json $options -f "$file" "$attribute")
   printf '%s' "$out" | jq -c '.[].outputs'
 else
-  out=$(nix build --no-link --json $(echo "$nix_options") "$attribute")
+  # shellcheck disable=SC2086
+  out=$(nix build --no-link --json $options "$attribute")
   printf '%s' "$out" | jq -c '.[].outputs'
 fi
