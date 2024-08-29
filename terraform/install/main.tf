a4cd5ed39456d5a4392ce07d2ce754ff320539a4
Michael Hoang <enzime@users.noreply.github.com>
2024-08-30 01:43:46 +1000

    terraform/install: fix `build_on_remote = false`
    Due to the Terraform variables being passed to `run-nixos-anywhere.sh`
    via environment variables, these environment variables wound up getting
    passed to `nixos-anywhere`.
    
    `nixos-anywhere` would then read the value `false` which would break
    everything as it expects the variable to be unset or set to `y`, leading
    to `disko_script` not being set.

--- terraform/install/main.tf
+++ terraform/install/main.tf
@@ -1,5 +1,20 @@
 locals {
   disk_encryption_key_scripts = [for k in var.disk_encryption_key_scripts : "\"${k.path}\" \"${k.script}\""]
+  arguments = jsonencode({
+    ssh_private_key = var.ssh_private_key
+    stop_after_disko = var.stop_after_disko
+    debug_logging = var.debug_logging
+    kexec_tarball_url = var.kexec_tarball_url
+    nixos_partitioner = var.nixos_partitioner
+    nixos_system = var.nixos_system
+    target_user = var.target_user
+    target_host = var.target_host
+    target_port = var.target_port
+    extra_files_script = var.extra_files_script
+    no_reboot = var.no_reboot
+    build_on_remote = var.build_on_remote
+    flake = var.flake
+  })
 }
 
 resource "null_resource" "nixos-remote" {
@@ -8,19 +23,7 @@ resource "null_resource" "nixos-remote" {
   }
   provisioner "local-exec" {
     environment = merge({
-      SSH_PRIVATE_KEY = var.ssh_private_key
-      stop_after_disko = var.stop_after_disko
-      debug_logging = var.debug_logging
-      kexec_tarball_url = var.kexec_tarball_url
-      nixos_partitioner = var.nixos_partitioner
-      nixos_system = var.nixos_system
-      target_user = var.target_user
-      target_host = var.target_host
-      target_port = var.target_port
-      extra_files_script = var.extra_files_script
-      no_reboot = var.no_reboot
-      build_on_remote = var.build_on_remote
-      flake = var.flake
+      ARGUMENTS = local.arguments
     }, var.extra_environment)
     command = "${path.module}/run-nixos-anywhere.sh ${join(" ", local.disk_encryption_key_scripts)}"
     quiet   = var.debug_logging
