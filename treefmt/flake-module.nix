c89b16bdaa06e33a5030097b94896a61d261ff4e
Jörg Thalheim <joerg@thalheim.io>
2024-09-26 18:36:40 +0200

    disable deno on darwin

--- treefmt/flake-module.nix
+++ treefmt/flake-module.nix
@@ -2,14 +2,14 @@
   imports = [
     inputs.treefmt-nix.flakeModule
   ];
-  perSystem = { config, ... }: {
+  perSystem = { config, pkgs, ... }: {
     treefmt = {
       projectRootFile = "flake.nix";
       programs.mdsh.enable = true;
       programs.nixpkgs-fmt.enable = true;
       programs.shellcheck.enable = true;
       programs.shfmt.enable = true;
-      programs.deno.enable = true;
+      programs.deno.enable = !pkgs.deno.meta.broken;
       settings.formatter.shellcheck.options = [ "-s" "bash" ];
     };
     formatter = config.treefmt.build.wrapper;
