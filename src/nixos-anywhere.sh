4b6413a12285f6d2bb2d5f75a019cd7148fedeff
ibizaman <ibizapeanut@gmail.com>
2025-04-09 20:20:50 +0200

    docs: clarify ssh-option flag usage

--- src/nixos-anywhere.sh
+++ src/nixos-anywhere.sh
@@ -85,7 +85,8 @@ Options:
 * -p, --ssh-port <ssh_port>
   set the ssh port to connect with
 * --ssh-option <ssh_option>
-  set an ssh option
+  set one ssh option, no need for the `-o` flag, can be repeated.
+  for example: `--ssh-option UserKnownHostsFile=./known_hosts`
 * -L, --print-build-logs
   print full build logs
 * --env-password
