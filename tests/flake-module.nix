1858c3ae4507961632a28e35326dd9e18fb15803
Jörg Thalheim <joerg@thalheim.io>
2025-06-01 09:33:40 +0200

    make system-to-install re-usuable for other tests
    this is needed for the ubuntu test.

--- tests/flake-module.nix
+++ tests/flake-module.nix
@@ -3,11 +3,16 @@
 {
   flake.checks.x86_64-linux = withSystem "x86_64-linux" ({ pkgs, system, inputs', config, ... }:
     let
+      system-to-install = pkgs.nixos [
+        ./modules/system-to-install.nix
+        inputs.disko.nixosModules.disko
+      ];
       testInputsUnstable = {
         inherit pkgs;
         inherit (inputs.disko.nixosModules) disko;
         nixos-anywhere = config.packages.nixos-anywhere;
         kexec-installer = "${inputs'.nixos-images.packages.kexec-installer-nixos-unstable-noninteractive}/nixos-kexec-installer-noninteractive-${system}.tar.gz";
+        inherit system-to-install;
       };
       testInputsStable = testInputsUnstable // {
         kexec-installer = "${inputs'.nixos-images.packages.kexec-installer-nixos-stable-noninteractive}/nixos-kexec-installer-noninteractive-${system}.tar.gz";
